34c88bc65f2693782078190549b6fa21ae315b10
joeratt <joerattazzi@gmail.com>
2013-04-16 22:53:13 -0500

    attempt 1 at google vs. apple. Hope it works

--- funprog/objsets/src/main/scala/objsets/TweetSet.scala
+++ funprog/objsets/src/main/scala/objsets/TweetSet.scala
@@ -208,8 +208,8 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = TweetReader.allTweets.filterAcc(((p: Tweet) => google.contains(p.text)), new Empty)
-  lazy val appleTweets: TweetSet = TweetReader.allTweets.filterAcc(((p: Tweet) => apple.contains(p.text)), new Empty)
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.filterAcc(((p: Tweet) => google.exists((str:String)=>p.text.contains(str))), new Empty)
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.filterAcc(((p: Tweet) => apple.exists((str:String)=>p.text.contains(str))), new Empty)
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
